d4cf1e8dd1669e2e2db73e67a6338e20926c6a3e
[Samir Jabrayilzada] <[samirej@code.edu.az]>
2022-12-23 19:28:52 +0400

    FirstLab

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace RepetitionStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 100)
+                Console.WriteLine(i++); 
+            //1den yuze kimi ededler (yeni i < 100)
+
+            }
+            
+        } 
+
+      
+    }
